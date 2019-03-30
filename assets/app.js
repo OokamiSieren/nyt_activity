ac0407b2143d823cb029d732f74f6252501f2cec
Andre Batista <andrebatista@Andres-MacBook-Air.local>
2019-03-30 11:49:41 -0400

    linked Js

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+<script>
+var apiKey = "fgWk0sChyJYVrgCUGqFkyYZopoyVjZcL";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + apiKey;
+   $.ajax({
+url: queryURL,
+method: "GET"
+}).then(function(response) {
+console.log(this);
+});
+</script>
