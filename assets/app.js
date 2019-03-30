5cc50e19b3be2f2352db74420bf969129c32b3a9
Andre Batista <andrebatista@Andres-MacBook-Air.local>
2019-03-30 12:19:39 -0400

    added correct link to jquery

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,13 @@
-<script>
-var apiKey = "fgWk0sChyJYVrgCUGqFkyYZopoyVjZcL";
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + apiKey;
-   $.ajax({
-url: queryURL,
-method: "GET"
-}).then(function(response) {
-console.log(this);
-});
-</script>
+$(document).ready(function(){
+
+        var apiKey = "fgWk0sChyJYVrgCUGqFkyYZopoyVjZcL";
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + apiKey;
+           $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function(response) {
+        console.log(response);
+        });
+    });
+    
